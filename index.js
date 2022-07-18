bf422f891b4b2044b1c840ee86b1c082b7a8902e
AishaBaigtu <aishabaigtu@gmail.com>
2022-07-18 16:16:47 +0800

    Add cors header

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 
+app.use(function (req, res, next) {
+
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
+    next();
+});
+
 mongoose.connect(process.env.DB_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
